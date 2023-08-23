e719e492e1c3b480c4fef48f00645285fdcd4b9c
DavidEtoedia <etoedia@gmail.com>
2023-08-23 15:05:40 -0700

    Initial Commit

---
+++ Fusion Watch App/FusionApp.swift
@@ -0,0 +1,17 @@
+//
+//  FusionApp.swift
+//  Fusion Watch App
+//
+//  Created by Inyene Etoedia on 23/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct Fusion_Watch_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
